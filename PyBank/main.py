a70fe9c8902084f882f2d88577f71abf7337348a
shenae <shenaeryan90@hotmail.com>
2023-10-23 17:36:56 +1030

    new file:   .vscode/settings.json 	modified:   PyBank/main.py 	new file:   PyPoll/election_results.txt 	modified:   PyPoll/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,33 @@
+import csv
+filename = "budget_data.csv"
 
+with open(filename, 'r') as file:
+    csvreader = csv.reader(file)
+    next(csvreader)
+    data = list(csvreader)
+
+    total_months = len(data)
+
+    profit_loss = [int(row[1]) for row in data]
+    net_total = sum(profit_loss)
+
+    changes = [profit_loss[i+1] - profit_loss[i] for i in range(len(data) - 1)]
+
+    average_change = sum(changes)/len(changes)
+
+    max_increase = max(changes)
+    max_decrease = min(changes)
+
+    max_increase_date = data[changes.index(max_increase)+1][0]
+    max_decrease_date = data[changes.index(max_decrease)+1][0]
+
+    print("financial Analysis")
+    print("__________________")
+    print(f"Total Months:{total_months}")
+    print(f"Total: ${net_total}")
+    print(f"Average Change:${average_change:.2f}")
+    print(f"Greatest Increase in Profits:{max_increase_date}(${max_increase})")
+    print(f"Greatest Decrease in Profits: {max_decrease_date}(${max_decrease})")
+
+
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,35 @@
+import pandas as pd
 
+file_path = "election_data.csv"
+election_data = pd.read_csv(file_path)
+
+total_votes = len(election_data)
+
+candidate_votes = election_data["Candidate"].value_counts().reset_index()
+candidate_votes.columns = ["Candidate", "Vote Count"]
+
+candidate_votes["Vote Percentage"] = (candidate_votes["Vote Count"] / total_votes) * 100
+
+winner = candidate_votes["Candidate"].iloc[0]
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+print(candidate_votes.to_string(index=False))
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+output_file = "election_results.txt"
+with open(output_file, "w") as file:
+    file.write("Election Results\n")
+    file.write("-------------------------\n")
+    file.write(f"Total Votes: {total_votes}\n")
+    file.write("-------------------------\n")
+    file.write(candidate_votes.to_string(index=False) + "\n")
+    file.write("-------------------------\n")
+    file.write(f"Winner: {winner}\n")
+    file.write("-------------------------\n")
+
+print(f"Results have been saved to {output_file}")
